74046ec96f04886c43ceab629c425c13309436fc
eramoss <edulramos@outlook.com>
2022-12-22 21:34:21 -0300

    fix error on click in label, no called function

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -1,44 +1,39 @@
-import * as Checkbox from '@radix-ui/react-checkbox';
-import { CheckIcon } from '@radix-ui/react-icons';
-import { ButtonHTMLAttributes, useContext } from 'react';
+import * as Checkbox from "@radix-ui/react-checkbox";
+import { CheckIcon } from "@radix-ui/react-icons";
+import React, { ButtonHTMLAttributes, useContext } from "react";
 
-
-export interface checkboxProps extends ButtonHTMLAttributes<HTMLButtonElement>{
-  children: string;
-  id: string;
+export interface checkboxProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  htmlfor: string;
+  onCheck: () => void;
 }
 
-
-export function CheckboxComponent({children,id, ...props }: checkboxProps){
-
-
-
-
-
-
-return (
-  <div>
-    <form>
-        <div className='items-center flex'>
-         
-          <Checkbox.Root className="bg-gray-900 w-6 h-6 rounded flex items-center justify-center shadow-sm" defaultChecked id={id}>
-            <Checkbox.Indicator >
+export function CheckboxComponent({
+  children,
+  htmlfor,
+  onCheck,
+  ...props
+}: checkboxProps) {
+  return (
+    <div>
+      <form>
+        <div className="items-center flex">
+          <Checkbox.Root
+            className="bg-gray-900 w-6 h-6 rounded flex items-center justify-center shadow-sm"
+            defaultChecked
+            id={htmlfor}
+            onClick={onCheck}
+          >
+            <Checkbox.Indicator>
               <CheckIcon />
             </Checkbox.Indicator>
           </Checkbox.Root>
 
-          <label className="mx-2" htmlFor={id}>
+          <label className="pl-2" htmlFor={htmlfor}>
             {children}
           </label>
-
         </div>
       </form>
     </div>
-    
-
-
-  )
+  );
 }
-
-
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
-
 import { Button } from "../components/ButtonComponent";
-import {CheckboxComponent} from "../components/Checkbox";
+import { CheckboxComponent } from "../components/Checkbox";
 import { HeaderBody } from "../components/HeaderBody";
 import { StrengthPass } from "../components/StrenghtComponent/StrengthPass";
 import { useContext, useState } from "react";
@@ -8,67 +7,51 @@ import { InpuComponent } from "../components/InputComponent";
 import Appcontext from "../context/AppContext";
 import { ModalCopy } from "../components/ModalCopy";
 
-
-
-
 export default function Home() {
-  
-
-
-  const {error,CheckLower,CheckNumber,CheckSymbol,CheckUpper } = useContext(Appcontext)
-
+  const { error, CheckLower, CheckNumber, CheckSymbol, CheckUpper } =
+    useContext(Appcontext);
 
   return (
-    <div className='flex justify-center items-center w-screen h-screen bg-slate-900 font-bold'>
-     
+    <div className="flex justify-center items-center w-screen h-screen bg-slate-900 font-bold">
       <div className="  fixed -right-36 top-4 ">
-        <ModalCopy/>
+        <ModalCopy />
       </div>
-        
 
-      
-      
       <div className="w-96 h-auto flex flex-col gap-4">
-          
-        <HeaderBody/>
+        <HeaderBody />
 
         <main className="flex flex-col justify-evenly  bg-slate-800    h-full w-full rounded p-4 ">
-          
-        
-
-  
-          <h1 className="text-red-700 " >{error}</h1>
-          <InpuComponent/>
-
-        
-
+          <h1 className="text-red-700 ">{error}</h1>
+          <InpuComponent />
 
           <div className="text-sm text-white my-4 flex flex-col gap-2">
-            <a onClick={CheckUpper}>     
-              <CheckboxComponent id='Upper' > Include Uppercase Letters </CheckboxComponent>
-            </a>
-            
-            <a onClick={CheckLower}>
-              <CheckboxComponent  id='Lower' > Include Lowercase Letters </CheckboxComponent>
-            </a>
-            
-            <a onClick={CheckNumber}>
-              <CheckboxComponent  id="Number" > Include Numbers </CheckboxComponent>
-            </a>
-            
-            <a onClick={CheckSymbol}>
-              <CheckboxComponent  id="Symbol" > Include Symbols </CheckboxComponent>
-            </a>
-          </div>
+            <CheckboxComponent onCheck={CheckUpper} htmlfor="Upper">
+              {" "}
+              Include Uppercase Letters{" "}
+            </CheckboxComponent>
 
+            <CheckboxComponent onCheck={CheckLower} htmlfor="Lower">
+              {" "}
+              Include Lowercase Letters{" "}
+            </CheckboxComponent>
+
+            <CheckboxComponent onCheck={CheckNumber} htmlfor="Number">
+              {" "}
+              Include Numbers{" "}
+            </CheckboxComponent>
+
+            <CheckboxComponent onCheck={CheckSymbol} htmlfor="Symbol">
+              {" "}
+              Include Symbols{" "}
+            </CheckboxComponent>
+          </div>
 
           <div>
-            <StrengthPass/>
+            <StrengthPass />
             <Button> GENERATE </Button>
           </div>
-
         </main>
       </div>
     </div>
-  )
+  );
 }
